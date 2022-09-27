9118c2f17d3bd0eb251282c19cd44ef52bcc887b
takahashi <ryo.takahashi.sys22@morijyobi.ac.jp>
2022-09-27 15:16:36 +0900

    pra2.javaに出力処理追記を完了しました

--- pra2.java
+++ pra2.java
@@ -1,6 +1,7 @@
 public class pra2{
 	public static void main(String[] args){
-
-	System.out.println("hello world");
+	for(int i = 1; i <= 100; i++){
+		System.out.println("hello world");
 	}
+	}	
 }
